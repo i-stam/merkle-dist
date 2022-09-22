b59a16c0ea9906a60d69468d03212ddbc577c7db
i-stam <i.stamelakos@gmail.com>
2022-09-22 15:58:26 +0300

    Add main contract

---
+++ test/MerkleDistributor.t.sol
@@ -0,0 +1,12 @@
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+import "src/MerkleDistributor.sol";
+
+contract MerkleDistributorTest is Test {
+    MerkleDistributor merkleDistributor;
+
+
+}
